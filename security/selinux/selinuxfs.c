6da5859df64b51b1870c85bed1190e40e9bc77ba
naosil <natstonsilalahi@gmail.com>
2026-09-16 20:52:22 +0700

    add sel_handle_status_ops export

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -261,7 +261,7 @@ static int sel_mmap_handle_status(struct file *filp,
 			       size, vma->vm_page_prot);
 }
 
-static const struct file_operations sel_handle_status_ops = {
+const struct file_operations sel_handle_status_ops = {
 	.open		= sel_open_handle_status,
 	.read		= sel_read_handle_status,
 	.mmap		= sel_mmap_handle_status,
